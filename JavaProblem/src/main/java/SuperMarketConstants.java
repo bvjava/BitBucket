dc5061166b3acb681d13a5aebbb79af57cc5b9de
bv <bvgitdevelopment@gmail.com>
2016-03-05 23:39:53 +0000

    Initial commit. Constants to use at many places to avoid redundancy.

---
+++ JavaProblem/src/main/java/SuperMarketConstants.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main.java;
+
+/**
+ * @author Bhakti
+ *
+ * Supermarket items constants file.
+ */
+public class SuperMarketConstants {
+
+	public static final String ORGANIC_BANANA ="Organic Banana";
+	public static final String THERMAL_VEST ="Thermal Vest";
+	public static final String AGED_BRIE ="Aged Brie";
+	public static final String CHICKEN ="Chicken";
+	public static final String SULFURAS ="Sulfuras";
+	public static final String BACKSTAGE_PASSES ="Backstage Passes";
+	public static final String GINGER_CAKE ="Ginger Cake";
+}
