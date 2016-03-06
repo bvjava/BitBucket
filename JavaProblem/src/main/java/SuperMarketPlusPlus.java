c63c20d10dd243f4558874a1bc844e106ee9895e
bv <bvgitdevelopment@gmail.com>
2016-03-06 00:27:20 +0000

    Implemented the logic for the new item "Organic Banana". Modified the existing code as per the requirements and added the corrections.

--- JavaProblem/src/main/java/SuperMarketPlusPlus.java
+++ JavaProblem/src/main/java/SuperMarketPlusPlus.java
@@ -24,12 +24,16 @@ public class SuperMarketPlusPlus {
         items.add(new Item(SuperMarketConstants.GINGER_CAKE, 3, 6));
         items.add(new Item(SuperMarketConstants.ORGANIC_BANANA, 6, 16)); // Addition of new item ORGANIC_BANANA to the existing system.
 
-        updateQuality();
+        try {
+			updateQuality();
+		} catch (SuperMarketException e) {
+			e.printStackTrace();
+		}
 }
 
 
 	
-    public static void updateQuality()
+    public static void updateQuality() throws SuperMarketException
     {
         for (int i = 0; i < items.size(); i++)
         {
@@ -40,7 +44,19 @@ public class SuperMarketPlusPlus {
                 {
                     if (!SuperMarketConstants.SULFURAS.equals(items.get(i).getName()))
                     {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
+                    	//New Implementation : If item is ORGANIC_BANANA then quality will decreased twice than normal items.
+                    	if(SuperMarketConstants.ORGANIC_BANANA.equals(items.get(i).getName())){
+                    		// Requirement quality can not be negative.
+                    		if(items.get(i).getQuality() > 1){
+                    			items.get(i).setQuality(items.get(i).getQuality() - 2);
+                    		}      
+                    		else{
+                    			items.get(i).setQuality(0);
+                    		}
+                    	}
+                    	else{
+                    		items.get(i).setQuality(items.get(i).getQuality() - 1);
+                    	}
                     }
                 }
             }
@@ -49,6 +65,11 @@ public class SuperMarketPlusPlus {
                 if (items.get(i).getQuality() < 50)
                 {
                     items.get(i).setQuality(items.get(i).getQuality() + 1);
+                    
+                    /* Modification in existing logic according to the requirement:
+                     * "Backstage Passes", like aged brie, increases in Quality as it's SellIn value reduces (i.e as it gets closer to the concert). 
+                     * Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
+                     */
 
                     if (SuperMarketConstants.BACKSTAGE_PASSES.equals(items.get(i).getName()))
                     {
@@ -56,7 +77,7 @@ public class SuperMarketPlusPlus {
                         {
                             if (items.get(i).getQuality() < 50)
                             {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                                items.get(i).setQuality(items.get(i).getQuality() + 2); //Correction. Previously increased by 1.
                             }
                         }
 
@@ -64,7 +85,7 @@ public class SuperMarketPlusPlus {
                         {
                             if (items.get(i).getQuality() < 50)
                             {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                                items.get(i).setQuality(items.get(i).getQuality() + 3); //Correction. Previously increased by 1.
                             }
                         }
                     }
@@ -73,7 +94,7 @@ public class SuperMarketPlusPlus {
 
             if (!SuperMarketConstants.SULFURAS.equals(items.get(i).getName()))
             {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
+            	items.get(i).setSellIn(items.get(i).getSellIn() - 1);
             }
 
             if (items.get(i).getSellIn() < 0)
@@ -86,7 +107,11 @@ public class SuperMarketPlusPlus {
                         {
                             if (!SuperMarketConstants.SULFURAS.equals(items.get(i).getName()))
                             {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
+                            	/* Modified the existing code to meet the requirement:
+                            	 * Once the sell by date has passed, Quality degrades twice as fast (i.e. the int is decremented by 2 instead of 1)
+                            	 * 
+                            	 */
+                                items.get(i).setQuality(items.get(i).getQuality() - 2); // Correction. Previously done by 1.
                             }
                         }
                     }
